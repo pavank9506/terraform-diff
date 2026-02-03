b774620608995bea34aba965dfd76df512a5113e
Your Name <pavan.kota439@email.com>
2026-02-03 22:48:22 +0530

    commting the messages

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform" {
 
     ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.small"
+    instance_type = "t2.micro"
     # vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
         Name = "terraform"
